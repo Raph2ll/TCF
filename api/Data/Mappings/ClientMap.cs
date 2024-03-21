1bac10f677320a8bab62d3ae87652bd84651b3d4
Raph2ll <raphael.ph2ll@gmail.com>
2024-03-21 19:58:55 -0300

    feat: client map

---
+++ api/Data/Mappings/ClientMap.cs
@@ -0,0 +1,32 @@
+using System;
+using api.Mappings.Interfaces;
+using MySql.Data.MySqlClient;
+using DotEnv;
+
+namespace api.Data.Mappings
+{
+    public class ClientMap : IEntityMap
+    {
+        string databaseName = Environment.GetEnvironmentVariable("DATABASE_NAME");
+        public void Configure(MySqlConnection connection)
+        {
+            using (var cmd = connection.CreateCommand())
+            {
+                cmd.CommandText = $"USE {databaseName}";
+                cmd.ExecuteNonQuery();
+
+                cmd.CommandText = @"
+                    CREATE TABLE IF NOT EXISTS client (
+                        id VARCHAR(40) PRIMARY KEY,
+                        name VARCHAR(80) NOT NULL,
+                        surname VARCHAR(80) NOT NULL,
+                        email VARCHAR(80) NOT NULL,
+                        birthdate DATE NOT NULL,
+                        created_at DATETIME NOT NULL,
+                        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+                    );";
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}

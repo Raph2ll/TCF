2151fd1cfcd1ed7c4545e9c7fa5f2d435ef49618
Raph2ll <raphael.ph2ll@gmail.com>
2024-03-21 19:44:21 -0300

    feat: DataContext

---
+++ api/Data/DataContext.cs
@@ -0,0 +1,45 @@
+using api.Models;
+using api.Mappings;
+
+using MySql.Data.MySqlClient;
+
+namespace api.Data
+{
+    public class DataContext
+    {
+        private readonly string _connectionString;
+        private readonly IEnumerable<IEntityMap> _entityMaps;
+        string databaseName = Environment.GetEnvironmentVariable("DATABASE_NAME");
+
+        public DataContext(string connectionString, IEnumerable<IEntityMap> entityMaps)
+        {
+            _connectionString = connectionString;
+            _entityMaps = entityMaps;
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(_connectionString);
+        }
+
+        public void OnModelCreating()
+        {
+            using (MySqlConnection connection = GetConnection())
+            {
+                connection.Open();
+
+                using (var cmd = connection.CreateCommand())
+                {
+                    cmd.CommandText = $"CREATE DATABASE IF NOT EXISTS {databaseName}";
+                    cmd.ExecuteNonQuery();
+                }
+                
+                foreach (var entityMap in _entityMaps)
+                {
+                    entityMap.Configure(connection);
+                }
+            }
+        }
+
+    }
+}

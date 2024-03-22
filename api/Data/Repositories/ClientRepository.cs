4af71e107051d7a8f59f243a8a22273442dc73bf
Raph2ll <raphael.ph2ll@gmail.com>
2024-03-22 14:46:05 -0300

    feat: Client Repository

---
+++ api/Data/Repositories/ClientRepository.cs
@@ -0,0 +1,141 @@
+using System;
+using System.Collections.Generic;
+using api.Data.Repositories.Interfaces;
+using api.Models;
+using MySql.Data.MySqlClient;
+
+namespace api.Data.Repositories
+{
+    public class ClientRepository : IClientrepository
+    {
+        private readonly DataContext _connection;
+        private string TableName = "client";
+
+        public ClientRepository(DataContext connection)
+        {
+            _connection = connection;
+        }
+
+        public void CreateClient(Client client)
+        {
+            using (var dbConnection = _connection.GetConnection())
+            {
+                dbConnection.Open();
+                using (var cmd = new MySqlCommand(@$"INSERT INTO {TableName} (id, name, surname, email, birthdate) 
+                    VALUES (@Id, @Name, @Surname, @Email, @BirthDate)",
+                    dbConnection))
+                {
+                    cmd.Parameters.AddWithValue("@Id", client.Id);
+                    cmd.Parameters.AddWithValue("@Name", client.Name);
+                    cmd.Parameters.AddWithValue("@Surname", client.Surname);
+                    cmd.Parameters.AddWithValue("@Email", client.Email);
+                    cmd.Parameters.AddWithValue("@BithDate", client.BrithDate);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public List<Client> GetClients()
+        {
+            var clients = new List<Client>();
+
+            using (var dbConnection = _connection.GetConnection())
+            {
+                dbConnection.Open();
+                using (var command = new MySqlCommand($"SELECT id, name, surname, email, birthdate, created_at, updated_at FROM {TableName}",
+                    dbConnection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var client = new Client
+                            {
+                                Id = reader["id"].ToString(),
+                                Name = reader["name"].ToString(),
+                                Surname = reader["surname"].ToString(),
+                                Email = reader["email"].ToString(),
+                                BrithDate = Convert.ToDateTime(reader["birthdate"]),
+                                CreatedAt = Convert.ToDateTime(reader["created_at"]),
+                                UpdatedAt = Convert.ToDateTime(reader["updated_at"]),
+                            };
+
+                            clients.Add(client);
+                        }
+                    }
+                }
+            }
+
+            return clients;
+
+        }
+
+        public Client GetClientById(string id)
+        {
+            using (var dbConnection = _connection.GetConnection())
+            {
+                dbConnection.Open();
+                using (var command = new MySqlCommand($"SELECT name, surname, email, birthdate, created_at, updated_at FROM {TableName} WHERE id = @Id",
+                    dbConnection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            return new Client
+                            {
+                                Id = reader["id"].ToString(),
+                                Name = reader["name"].ToString(),
+                                Surname = reader["surname"].ToString(),
+                                Email = reader["email"].ToString(),
+                                BrithDate = Convert.ToDateTime(reader["birthdate"]),
+                                CreatedAt = Convert.ToDateTime(reader["created_at"]),
+                                UpdatedAt = Convert.ToDateTime(reader["updated_at"]),
+                            };
+                        }
+                    }
+                }
+            }
+
+            return null;
+
+        }
+
+        public void UpdateClient(Client updatedClient)
+        {
+            using (var dbConnection = _connection.GetConnection())
+            {
+                dbConnection.Open();
+                using (var cmd = new MySqlCommand($"UPDATE {TableName} SET name = @Name, surname = @Surname, email = @Email, birthdate = @BirthDate WHERE Id = @Id",
+                    dbConnection))
+                {
+                    cmd.Parameters.AddWithValue("@Id", updatedClient.Id);
+                    cmd.Parameters.AddWithValue("@Name", updatedClient.Name);
+                    cmd.Parameters.AddWithValue("@Surname", updatedClient.Surname);
+                    cmd.Parameters.AddWithValue("@Email", updatedClient.Email);
+                    cmd.Parameters.AddWithValue("@BithDate", updatedClient.BrithDate);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+        
+        public void DeleteClient(string id)
+        {
+            using (var dbConnection = _connection.GetConnection())
+            {
+                dbConnection.Open();
+                using (var command = new MySqlCommand($"DELETE FROM {TableName} WHERE id = @Id",
+                    dbConnection))
+                {
+                    command.Parameters.AddWithValue("@Id", id);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ api/Data/Repositories/Interfaces/IClientrepository.cs
@@ -0,0 +1,14 @@
+using System;
+using api.Models;
+
+namespace api.Data.Repositories.Interfaces
+{
+    public interface IClientrepository
+    {
+        public void CreateClient(Client client);
+        public List<Client> GetClients();
+        public Client GetClientById(string id);
+        public void UpdateClient(Client updatedClient);
+        public void DeleteClient(string id);
+    }
+}

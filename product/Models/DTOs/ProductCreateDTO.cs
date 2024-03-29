baa461bbf170e70dbd1904605323f71d205bc784
Raph2ll <raphael.ph2ll@gmail.com>
2024-03-29 17:47:45 -0300

    feat: product create DTO

--- product/Models/DTOs/ProductCreateDTO.cs
+++ product/Models/DTOs/ProductCreateDTO.cs
@@ -2,5 +2,8 @@ namespace product.Models.DTOs;
 
 public class ProductCreateDTO
 {
-    
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int Quantity { get; set; }
+    public decimal Price { get; set; }
 }

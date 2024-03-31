282550d60e6d3cca938db52bef9d63b9fe65934b
Raph2ll <raphael.ph2ll@gmail.com>
2024-03-31 10:34:02 -0300

    fix: namespaces

--- product/Exceptions/NotFoundException.cs
+++ product/Exceptions/NotFoundException.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace client.Exceptions
+namespace product.Exceptions
 {
     public class NotFoundException : Exception
     {

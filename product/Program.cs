4650560a313ece25613bc4ed90433a76859e9e0f
Raph2ll <raphael.ph2ll@gmail.com>
2024-04-02 17:39:47 -0300

    feat: product program

--- product/Program.cs
+++ product/Program.cs
@@ -1,15 +1,75 @@
-namespace product;
+using product.Data;
+using product.Data.Mappings.Interfaces;
+using product.Data.Mappings;
+using product.Data.Repositories.Interfaces;
+using product.Data.Repositories;
+using product.Services.Interfaces;
+using product.Services;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Collections.Generic;
 
-public class Program
+namespace product
 {
-    public static void Main(string[] args)
+    public class Program
     {
-        var builder = WebApplication.CreateBuilder(args);
-        var app = builder.Build();
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            
+            var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
 
-        app.MapGet("/", () => "Hello World!");
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
 
-        var port = "7070";
-        app.Run($"http://0.0.0.0:{port}");
+            builder.Services.AddSingleton<DataContext>(_ =>
+            {
+                var entityMaps = new List<IEntityMap>
+                {
+                    new ProductMap()
+                };
+
+                var connection = new DataContext(connectionString, entityMaps);
+                connection.OnModelCreating();
+
+                return connection;
+            });
+
+            builder.Services.AddSingleton<IProductRepository, ProductRepository>();
+            builder.Services.AddSingleton<IProductService, ProductService>();
+
+            builder.Services.AddEndpointsApiExplorer();
+
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "product", Version = "v1" });
+            });
+
+            builder.Services.AddControllers();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "product");
+                });
+            }
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            var port = "7070";
+            app.Run($"http://0.0.0.0:{port}");
+        }
     }
 }

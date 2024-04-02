4253cf324a671f9a2e8a79e43b24354764621af7
Raph2ll <raphael.ph2ll@gmail.com>
2024-04-02 19:51:49 -0300

    feat: doc file

--- client/Program.cs
+++ client/Program.cs
@@ -1,16 +1,18 @@
-using Microsoft.OpenApi.Models;
 using client.Data;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using client.Data.Mappings.Interfaces;
 using client.Data.Mappings;
 using client.Data.Repositories.Interfaces;
 using client.Data.Repositories;
 using client.Services.Interfaces;
 using client.Services;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System.Collections.Generic;
+using System.Reflection;
+
 
 namespace client
 {
@@ -38,19 +40,34 @@ namespace client
 
                 return connection;
             });
-            
+
             builder.Services.AddSingleton<IClientRepository, ClientRepository>();
             builder.Services.AddSingleton<IClientService, ClientService>();
-        
+
             builder.Services.AddEndpointsApiExplorer();
-        
+
             builder.Services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "client", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "client",
+                    Version = "v1",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Raph2ll",
+                        Email = "raph2ll@gmail.com",
+                        Url = new Uri("https://github.com/Raph2ll")
+                    }
+                });
+                
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+
             });
-            
+
             builder.Services.AddControllers();
-            
+
             var app = builder.Build();
 
             if (app.Environment.IsDevelopment())
@@ -64,13 +81,10 @@ namespace client
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers(); 
-            });
+            app.MapControllers();
 
-        var port = "8080";
-        app.Run($"http://0.0.0.0:{port}");
+            var port = "8080";
+            app.Run($"http://0.0.0.0:{port}");
         }
     }
 }

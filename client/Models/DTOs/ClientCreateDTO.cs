99a00de069f69cc06bdee0563fb9d576b3f6a95a
Raph2ll <raphael.ph2ll@gmail.com>
2024-04-02 20:50:08 -0300

    feat: new Client DTO

--- client/Models/DTOs/ClientCreateDTO.cs
+++ client/Models/DTOs/ClientCreateDTO.cs
@@ -2,18 +2,23 @@ using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualBasic;
 using System.Text.Json.Serialization;
+using client.Models.DataAnnotations;
 
 namespace client.Models.DTOs
 {
     public class ClientCreateDTO
     {
-        [Required]
-        public string Name { get; set; }
-        [Required]
-        public string Surname { get; set; }
-        [Required]
-        public string Email { get; set; }
-        [Required]
+        [Required, MinLength(3), MaxLength(80)]
+        public string? Name { get; set; }
+
+        [Required, MinLength(3), MaxLength(80)]
+
+        public string? Surname { get; set; }
+
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required, DataType(DataType.Date), DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true), CustomerDateOfBirthValidation]
         public DateTime BirthDate { get; set; }
     }
 }

262945a26dcff8459942f8c9f411dc32173dfeaa
Raph2ll <raphael.ph2ll@gmail.com>
2024-04-04 14:19:49 -0300

    fix: custom validation

--- client/Models/DataAnnotations/CustomerDateOfBirthValidation.cs
+++ client/Models/DataAnnotations/CustomerDateOfBirthValidation.cs
@@ -3,27 +3,16 @@ using System.ComponentModel.DataAnnotations;
 
 namespace client.Models.DataAnnotations
 {
-
     public class CustomerDateOfBirthValidation : ValidationAttribute
     {
-        public const string MINIMUM_DATE_OF_BIRTH = "The customer is younger than 18 years old";
-        public const string MAXIMUM_DATE_OF_BIRTH = "The customer is older than 200 years old";
 
         protected override ValidationResult IsValid(object value, ValidationContext validationContext)
         {
-            if (value == null || !(value is DateTime))
-            {
-                return new ValidationResult("Invalid date of birth format.");
-            }
-
             DateTime dob = (DateTime)value;
 
-            var currentDate = DateTime.Now.Date;
-            var oldestAllowedDateOfBirth = currentDate.AddYears(-200);
-
-            if (dob > currentDate || dob < oldestAllowedDateOfBirth)
+            if (value == null || !(value is DateTime) || dob <= DateTime.MinValue.Date)
             {
-                return new ValidationResult("Date of birth is outside the allowed range.");
+                return new ValidationResult("Date of birth is required.");
             }
 
             return ValidationResult.Success;

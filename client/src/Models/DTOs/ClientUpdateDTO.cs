0585c112fd75e9a54e4a701680289b2b8e592213
Raph2ll <raphael.ph2ll@gmail.com>
2024-04-16 14:23:03 -0300

    fix: ClientUpdateDTO

--- client/src/Models/DTOs/ClientUpdateDTO.cs
+++ client/src/Models/DTOs/ClientUpdateDTO.cs
@@ -2,14 +2,24 @@ using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualBasic;
 using System.Text.Json.Serialization;
+using client.Models.DataAnnotations;
+
 
 namespace client.Models.DTOs
 {
     public class ClientUpdateDTO
     {
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public string Email { get; set; }
+        [MinLength(3), MaxLength(80)]
+        public string? Name { get; set; }
+
+        [MinLength(3), MaxLength(80)]
+
+        public string? Surname { get; set; }
+
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [DataType(DataType.Date), CustomerDateOfBirthValidation]
         public DateTime BirthDate { get; set; }
     }
 }

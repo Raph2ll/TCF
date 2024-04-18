23c23f250163efe08c090448a0b583d234ba39d4
Raph2ll <raphael.ph2ll@gmail.com>
2024-04-18 13:29:26 -0300

    feat: Logger in product service

--- product/Services/ProductService.cs
+++ product/Services/ProductService.cs
@@ -3,94 +3,130 @@ using product.Db.Repositories.Interfaces;
 using product.Models;
 using product.Models.DTOs;
 using product.Services.Interfaces;
+using Serilog;
+using System.Diagnostics;
+using System.Reflection;
+using product.Utils;
+
 
 namespace product.Services
 {
     public class ProductService : IProductService
     {
         private readonly IProductRepository _productRepository;
+        private readonly Serilog.ILogger _logger;
+        private readonly ContextFactory _ctxFactory;
+        private readonly string _namespace = "Service";
+
 
         public ProductService(IProductRepository productRepository)
         {
             _productRepository = productRepository;
+            _logger = Serilog.Log.ForContext<ProductService>();
+            _ctxFactory = new ContextFactory(_logger);
         }
 
         public void CreateProduct(ProductCreateDTO newProduct)
         {
-            var product = new Product()
-            {
-                Id = Guid.NewGuid().ToString(),
-                Name = newProduct.Name,
-                Description = newProduct.Description,
-                Quantity = newProduct.Quantity,
-                Price = newProduct.Price
-            };
+            var methodName = $"{_namespace} {MethodBase.GetCurrentMethod()!.Name}";
 
-            _productRepository.CreateProduct(product);
+            using (var ctx = _ctxFactory.Create(methodName))
+            {
+                var product = new Product()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = newProduct.Name,
+                    Description = newProduct.Description,
+                    Quantity = newProduct.Quantity,
+                    Price = newProduct.Price
+                };
+
+                _productRepository.CreateProduct(product);
+            }
+
         }
 
         public List<Product> GetProducts()
         {
-            return _productRepository.GetProducts();
-        }
+            var methodName = $"{_namespace} {MethodBase.GetCurrentMethod()!.Name}";
 
+            using (var ctx = _ctxFactory.Create(methodName))
+            {
+                return _productRepository.GetProducts();
+            }
+        }
         public Product GetProductById(string id)
         {
-            return _productRepository.GetProductById(id);
+            var methodName = $"{_namespace} {MethodBase.GetCurrentMethod()!.Name}";
+
+            using (var ctx = _ctxFactory.Create(methodName))
+            {
+                return _productRepository.GetProductById(id);
+            }
         }
 
         public void UpdateProduct(string id, ProductUpdateDTO updatedProductDto)
         {
-            var existingProduct = GetProductById(id);
+            var methodName = $"{_namespace} {MethodBase.GetCurrentMethod()!.Name}";
 
-            if (existingProduct == null)
+            using (var ctx = _ctxFactory.Create(methodName))
             {
-                throw new NotFoundException("Product Not Found");
+                var existingProduct = GetProductById(id);
+
+                if (existingProduct == null)
+                {
+                    throw new NotFoundException("Product Not Found");
+                }
+
+                string updatedName = existingProduct.Name;
+                if (!string.IsNullOrEmpty(updatedProductDto.Name))
+                {
+                    updatedName = updatedProductDto.Name;
+                }
+
+                string updatedDescription = existingProduct.Description;
+                if (!string.IsNullOrEmpty(updatedProductDto.Description))
+                {
+                    updatedDescription = updatedProductDto.Description;
+                }
+
+                int updatedQuantity = existingProduct.Quantity;
+                if (updatedProductDto.Quantity > 0)
+                {
+                    updatedQuantity = updatedProductDto.Quantity;
+                }
+
+                decimal updatedPrice = existingProduct.Price;
+                if (updatedProductDto.Price > 0)
+                {
+                    updatedPrice = updatedProductDto.Price;
+                }
+
+                var updatedProduct = new Product
+                {
+                    Id = existingProduct.Id,
+                    Name = updatedName,
+                    Description = updatedDescription,
+                    Quantity = updatedQuantity,
+                    Price = updatedPrice
+                };
+
+                _productRepository.UpdateProduct(updatedProduct);
             }
-
-            string updatedName = existingProduct.Name;
-            if (!string.IsNullOrEmpty(updatedProductDto.Name))
-            {
-                updatedName = updatedProductDto.Name;
-            }
-
-            string updatedDescription = existingProduct.Description;
-            if (!string.IsNullOrEmpty(updatedProductDto.Description))
-            {
-                updatedDescription = updatedProductDto.Description;
-            }
-
-            int updatedQuantity = existingProduct.Quantity;
-            if (updatedProductDto.Quantity > 0)
-            {
-                updatedQuantity = updatedProductDto.Quantity;
-            }
-
-            decimal updatedPrice = existingProduct.Price;
-            if (updatedProductDto.Price > 0)
-            {
-                updatedPrice = updatedProductDto.Price;
-            }
-
-            var updatedProduct = new Product
-            {
-                Id = existingProduct.Id,
-                Name = updatedName,
-                Description = updatedDescription,
-                Quantity = updatedQuantity,
-                Price = updatedPrice
-            };
-
-            _productRepository.UpdateProduct(updatedProduct);
         }
         public void DeleteProduct(string id)
         {
-            if (GetProductById(id) == null)
-            {
-                throw new NotFoundException("Client Not Found");
-            }
+            var methodName = $"{_namespace} {MethodBase.GetCurrentMethod()!.Name}";
 
-            _productRepository.DeleteProduct(id);
+            using (var ctx = _ctxFactory.Create(methodName))
+            {
+                if (GetProductById(id) == null)
+                {
+                    throw new NotFoundException("Client Not Found");
+                }
+
+                _productRepository.DeleteProduct(id);
+            }
         }
     }
 }

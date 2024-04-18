f447a7e00eaff36b6abbab37c76471fff2db8e8d
Raph2ll <raphael.ph2ll@gmail.com>
2024-04-18 14:03:10 -0300

    feat: more swagger configs and Logs

--- product/Program.cs
+++ product/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System.Collections.Generic;
+using System.Reflection;
 using Serilog;
 
 namespace product
@@ -18,7 +18,7 @@ namespace product
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            
+
             var configuration = new ConfigurationBuilder()
             .AddJsonFile("appsettings.json")
             .Build();
@@ -37,11 +37,38 @@ namespace product
 
             builder.Services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "product", Version = "v1" });
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "product",
+                    Version = "v1",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Raph2ll",
+                        Email = "raph2ll@gmail.com",
+                        Url = new Uri("https://github.com/Raph2ll")
+                    }
+                });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
             });
 
             builder.Services.AddControllers();
 
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Console(outputTemplate:
+                "{Timestamp:yyyy-MM-ddTHH:mm:ssZ} {Level:u}\t{Message:lj} {NewLine}{Exception}")
+                .Enrich.FromLogContext()
+                .CreateLogger();
+
+            builder.Services.AddLogging(loggingBuilder =>
+            {
+                loggingBuilder.AddSerilog(dispose: true);
+            });
+
+            builder.Host.UseSerilog();
+            
             var app = builder.Build();
 
             if (app.Environment.IsDevelopment())
@@ -52,13 +79,10 @@ namespace product
                     c.SwaggerEndpoint("/swagger/v1/swagger.json", "product");
                 });
             }
-
+            app.UseSerilogRequestLogging();
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
+            app.MapControllers();
             var port = "7070";
             app.Run($"http://0.0.0.0:{port}");
         }

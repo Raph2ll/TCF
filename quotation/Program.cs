69fe94768c78c8a4e1085427463fb2f8b03cdc83
Raph2ll <raphael.ph2ll@gmail.com>
2024-04-20 15:10:08 -0300

    feat: program

--- quotation/Program.cs
+++ quotation/Program.cs
@@ -1,6 +1,86 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.OpenApi.Models;
+using System.Reflection;
+using Serilog;
+using quotation.src.Services.Refit;
+using Refit;
+using quotation.src.Services.Interfaces;
+using quotation.src.Services;
 
-app.MapGet("/", () => "Hello World!");
 
-app.Run();
+namespace quotation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+            string awesomeApiUrl = configuration["External:AwesomeApi"];
+
+            builder.Services.AddSingleton<IQuotationService, QuotationService>();
+
+            builder.Services.AddEndpointsApiExplorer();
+
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "client",
+                    Version = "v1",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Raph2ll",
+                        Email = "raph2ll@gmail.com",
+                        Url = new Uri("https://github.com/Raph2ll")
+                    }
+                });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
+            builder.Services.AddControllers();
+            builder.Services.AddRefitClient<IEconomy>()
+                .ConfigureHttpClient(c => c.BaseAddress = new Uri(awesomeApiUrl));
+
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Console(outputTemplate:
+                "{Timestamp:yyyy-MM-ddTHH:mm:ssZ} {Level:u}\t{Message:lj} {NewLine}{Exception}")
+                .Enrich.FromLogContext()
+                .CreateLogger();
+
+            builder.Services.AddLogging(loggingBuilder =>
+            {
+                loggingBuilder.AddSerilog(dispose: true);
+            });
+
+            builder.Host.UseSerilog();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "client");
+                });
+            }
+
+            app.UseSerilogRequestLogging();
+            app.UseRouting();
+
+            app.MapControllers();
+
+            var port = "6060";
+            app.Run($"http://0.0.0.0:{port}");
+        }
+    }
+}

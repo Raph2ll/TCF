1ea83cdaecf51ea675ebb539badb2f37485e538c
Raph2ll <raphael.ph2ll@gmail.com>
2024-04-30 15:40:20 -0300

    fix: service interface

--- product/Services/Interfaces/IProductService.cs
+++ product/Services/Interfaces/IProductService.cs
@@ -7,7 +7,7 @@ namespace product.Services.Interfaces;
 public interface IProductService
 {
     public Product CreateProduct(ProductCreateDTO newProduct);
-    public List<Product> GetProducts();
+    public Task<List<ProductResponseDTO>> GetProducts();
     public Product GetProductById(string id);
     public void UpdateProduct(string id, ProductUpdateDTO updatedProductDto);
     public void DeleteProduct(string id);

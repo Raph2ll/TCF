fbab6c62990e9d3970b36c7e9b50e61031f5f7a1
Raph2ll <raphael.ph2ll@gmail.com>
2024-05-03 09:40:18 -0300

    feat: sale model

---
+++ sales/src/Models/Sale.cs
@@ -0,0 +1,25 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using sales.src.Models;
+
+namespace sales.src.Models
+{
+    public class Sale
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        public string? ClientId { get; set; }
+
+        public List<SaleItem>? Items { get; set; }
+
+        public int Status { get; set; }
+
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        public DateTime CreatedAt { get; set; }
+
+        [BsonDateTimeOptions(Kind = DateTimeKind.Utc)]
+        public DateTime UpdatedAt { get; set; }
+    }
+}

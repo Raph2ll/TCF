79f965a348c8f885c5dc62b08e8ad7f4643f061a
Raph2ll <raphael.ph2ll@gmail.com>
2024-05-04 16:17:51 -0300

    feat: "program"

--- sales/Program.cs
+++ sales/Program.cs
@@ -1,6 +1,89 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Reflection;
+using Serilog;
+using Refit;
 
-app.MapGet("/", () => "Hello World!");
 
-app.Run();
+namespace sales
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            /* string quotationApi = configuration["External:QuotationApi"];
+
+
+            builder.Services.AddSingleton<IMongoClient>(new MongoClient(connectionString));
+            builder.Services.AddSingleton<IProductRepository, ProductRepository>();
+            builder.Services.AddSingleton<IProductService, ProductService>();
+
+            builder.Services.AddEndpointsApiExplorer();
+
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "product",
+                    Version = "v1",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Raph2ll",
+                        Email = "raph2ll@gmail.com",
+                        Url = new Uri("https://github.com/Raph2ll")
+                    }
+                });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
+
+            builder.Services.AddControllers();
+            builder.Services.AddRefitClient<IQuotation>()
+                .ConfigureHttpClient(c => c.BaseAddress = new Uri(quotationApi));
+
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Information()
+                .WriteTo.Console(outputTemplate:
+                "{Timestamp:yyyy-MM-ddTHH:mm:ssZ} {Level:u}\t{Message:lj} {NewLine}{Exception}")
+                .Enrich.FromLogContext()
+                .CreateLogger();
+
+            builder.Services.AddLogging(loggingBuilder =>
+            {
+                loggingBuilder.AddSerilog(dispose: true);
+            });
+
+            builder.Host.UseSerilog();
+            
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "product");
+                });
+            }
+            app.UseSerilogRequestLogging();
+            app.UseRouting();
+
+            app.MapControllers();
+            var port = "7070";
+            app.Run($"http://0.0.0.0:{port}");
+            */
+        }
+    }
+}

a4c374c3597afb75e2d67b3b57dee8727f17faa5
Raph2ll <raphael.ph2ll@gmail.com>
2024-05-07 16:13:31 -0300

    fix: SaleItem Id

--- sales/src/Models/SaleItem.cs
+++ sales/src/Models/SaleItem.cs
@@ -1,9 +1,14 @@
 using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
 
 namespace sales.src.Models
 {
     public class SaleItem
     {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
         public string? SellId { get; set; }
 
         public string? ProductId { get; set; }

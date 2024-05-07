4ed4f3e305179fb7e45d424408ea44639b1da10e
Raph2ll <raphael.ph2ll@gmail.com>
2024-05-07 15:02:51 -0300

    fix: spacemenet and attributes

--- quotation/src/Models/Quotation.cs
+++ quotation/src/Models/Quotation.cs
@@ -8,9 +8,13 @@ namespace quotation.src.Models
 {
     public class Quotation
     {
+        [JsonPropertyName("EUR")]
         public decimal EUR { get; set; }
+        [JsonPropertyName("USD")]
         public decimal USD { get; set; }
+        [JsonPropertyName("GBP")]
         public decimal GBP { get; set; }
+        [JsonPropertyName("CNY")]
         public decimal CNY { get; set; }
         public DateTime UpdatedAt { get; set; }
 

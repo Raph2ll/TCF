671ebb5fe31be85ae37b1e1808ac2d68ae94020a
Raph2ll <raphael.ph2ll@gmail.com>
2024-05-11 14:05:53 -0300

    fix: remove not usued contet

--- client/src/Data/Repositories/FakeRepository.cs
+++ client/src/Data/Repositories/FakeRepository.cs
@@ -12,33 +12,27 @@ namespace client.src.Data.Repositories
 
         public void CreateClient(Client client)
         {
-            // Atribuir um novo ID ao cliente
             client.Id = Guid.NewGuid().ToString();
 
-            // Adicionar o cliente à lista
             _clients.Add(client);
 
         }
 
         public List<Client> GetClients()
         {
-            // Retornar todos os clientes na lista
             return _clients;
         }
 
         public Client GetClientById(string id)
         {
-            // Encontrar e retornar o cliente pelo ID
             return _clients.FirstOrDefault(c => c.Id == id);
         }
 
         public void UpdateClient(Client updatedClient)
         {
-            // Encontrar o cliente na lista pelo ID
             var existingClient = _clients.FirstOrDefault(c => c.Id == updatedClient.Id);
             if (existingClient != null)
             {
-                // Atualizar os dados do cliente existente com os dados atualizados
                 existingClient.Name = updatedClient.Name;
                 existingClient.Surname = updatedClient.Surname;
                 existingClient.Email = updatedClient.Email;
@@ -48,7 +42,6 @@ namespace client.src.Data.Repositories
 
         public void DeleteClient(string id)
         {
-            // Remover o cliente da lista pelo ID
             _clients.RemoveAll(c => c.Id == id);
         }
     }

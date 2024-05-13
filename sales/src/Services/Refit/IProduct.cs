09087a0c69bf3bf71f6bbbbd376a14437fe10cc1
Raph2ll <raphael.ph2ll@gmail.com>
2024-05-13 18:30:18 -0300

    feat: IProduct

---
+++ sales/src/Services/Refit/IProduct.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Refit;
+
+namespace sales.src.Services.Refit
+{
+    public interface IProduct
+    {
+        [Get("api/product/{id}")]
+        Task<ApiResponse<Product>> GetProductById([AliasAs("id")] string id);
+    }
+
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public bool Deleted { get; set; }
+    }
+}

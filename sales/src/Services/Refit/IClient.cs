ed888fad59dfbc05de5897c3770d765bd1ad9727
Raph2ll <raphael.ph2ll@gmail.com>
2024-05-24 17:41:08 -0300

    fix: refit request ObjectId

--- sales/src/Services/Refit/IClient.cs
+++ sales/src/Services/Refit/IClient.cs
@@ -2,21 +2,22 @@ using System;
 using System.Collections.Generic;
 using Refit;
 using System.Text.Json.Serialization;
+using MongoDB.Bson;
 
 namespace sales.src.Services.Refit
 {
     public interface IClient
     {
         [Get("/api/{id}")]
-        Task<ApiResponse<Client>> GetClientById([AliasAs("id")] string id);
+        Task<ApiResponse<Client>> GetClientById([AliasAs("id")] ObjectId id);
     }
 
     public class Client
     {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public string Email { get; set; }
+        public string? Id { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public string? Email { get; set; }
         public DateTime BirthDate { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }

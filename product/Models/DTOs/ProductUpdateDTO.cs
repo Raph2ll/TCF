8e4099bd2d9c5215589f127960189b3e8cc4af46
Raph2ll <raphael.ph2ll@gmail.com>
2024-05-26 18:12:29 -0300

    fix: productUpdateDTO

--- product/Models/DTOs/ProductUpdateDTO.cs
+++ product/Models/DTOs/ProductUpdateDTO.cs
@@ -7,8 +7,8 @@ namespace product.Models.DTOs
 {
     public class ProductUpdateDTO
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
         public int Quantity { get; set; }
         public decimal Price { get; set; }
     }
